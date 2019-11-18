8d19ddbeaf4871494e571b43cbff1e14121059ce
rudxde <kj4derechte@gmail.com>
2019-11-18 08:58:32 +0100

    add example

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Browser } from "./browser";
-import { Clickable } from "./wait-for";
+import { Clickable, Visible, BrowserIsReady, UrlContain, TitleContain, Invisible, Or, UrlIs } from "./wait-for";
+import { Key } from "./keys";
 function it(description: string, funciton: (b: Browser) => Promise<any>) { }
 
 class StartPage {
@@ -10,15 +11,22 @@ class StartPage {
 
 async () => {
     it('should navigate to google.de',
-        b => b
-            .restart()
-            .navigate('google.de')
+        async browser => {
+            await browser
+                .restart()
+                .wait(BrowserIsReady)
+                .navigate('google.de')
+                .expect(TitleContain('Google'))
+                .element('ibput.search')
+                .sendKeys('Meine Suche', Key.ENTER);
+            await browser.wait(Or(UrlContain('/search?q='), UrlContain('/404')));
+            
+        }
     );
 
     it('should click searchbar',
         b => new StartPage(b)
             .getSearchBar()
-            .click()
             .all('li')
             .forEach(e => e.wait(Clickable).click())
     )
