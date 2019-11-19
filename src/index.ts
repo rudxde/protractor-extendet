af4f4c21325b5a86bf2407d7cc92d1aae953eb1e
rudxde <kj4derechte@gmail.com>
2019-11-19 09:17:34 +0100

    setup exporting index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,5 @@
-import { Browser } from "./browser";
-import { Clickable, Visible, BrowserIsReady, UrlContain, TitleContain, Invisible, Or, UrlIs } from "./wait-for";
-import { Key } from "./keys";
-function it(description: string, funciton: (b: Browser) => Promise<any>) { }
-
-class StartPage {
-    constructor(private browser: Browser) { }
-    getSearchBar = () => this.browser.element('.seatch');
-    getSearchButton = () => this.browser.element('button[type=submit]');
-}
-
-async () => {
-    it('should navigate to google.de',
-        async browser => {
-            await browser
-                .restart()
-                .wait(BrowserIsReady)
-                .navigate('google.de')
-                .expect(TitleContain('Google'))
-                .element('ibput.search')
-                .sendKeys('Meine Suche', Key.ENTER);
-            await browser.wait(Or(UrlContain('/search?q='), UrlContain('/404')));
-            
-        }
-    );
-
-    it('should click searchbar',
-        b => new StartPage(b)
-            .getSearchBar()
-            .all('li')
-            .forEach(e => e.wait(Clickable).click())
-    )
-
-}
-
+export { Browser, BrowserPromise } from './browser';
+export { Element, ElementPromise } from './element';
+export { ElementArray, ElementArrayPromise } from './element-array';
+export { Key } from './keys';
+export * from './wait-for'
